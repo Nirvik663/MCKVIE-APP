eab05046ab1dbdb8183bcbf2fa2836aaf557d622
tclubmckvie <tclubmckvie@gmail.com>
2019-01-19 13:42:59 +0530

    Notice/News/Events Slider (Completed)

--- app/src/main/java/com/techclub/mckvie/Tab2.java
+++ app/src/main/java/com/techclub/mckvie/Tab2.java
@@ -5,12 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
@@ -18,30 +18,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Tab2.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Tab2#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Tab2 extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private RecyclerView mPeopleRV;
+    private DatabaseReference mDatabase;
+    private FirebaseRecyclerAdapter<object, Tab2.NewsViewHolder> mPeopleRVAdapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-
-    private RecyclerView mPeopleRV;
-    private DatabaseReference mDatabase;
-    private FirebaseRecyclerAdapter<object, notices.NewsViewHolder> mPeopleRVAdapter;
 
     public Tab2() {
         // Required empty public constructor
@@ -78,7 +68,62 @@ public class Tab2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab2, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab2, container, false);
+        final Context context = getActivity().getApplicationContext();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("News");
+        mDatabase.keepSynced(true);
+        mPeopleRV = (RecyclerView) view.findViewById(R.id.myRecycleView1);
+
+        DatabaseReference personsRef = FirebaseDatabase.getInstance().getReference().child("News");
+        Query personsQuery = personsRef.orderByKey();
+
+        mPeopleRV.hasFixedSize();
+        mPeopleRV.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+
+        FirebaseRecyclerOptions personsOptions = new FirebaseRecyclerOptions.Builder<object>().setQuery(personsQuery, object.class).build();
+
+        mPeopleRVAdapter = new FirebaseRecyclerAdapter<object, Tab2.NewsViewHolder>(personsOptions) {
+            @Override
+            protected void onBindViewHolder(Tab2.NewsViewHolder holder, final int position, final object model) {
+                holder.setTitle(model.getTitle());
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final String url = model.getUrl();
+                        Intent intent = new Intent(context, webview.class);
+                        intent.putExtra("id", url);
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public Tab2.NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.home_row, parent, false);
+
+                return new Tab2.NewsViewHolder(view);
+            }
+        };
+
+        mPeopleRV.setAdapter(mPeopleRVAdapter);
+
+        return view;
+    }
+
+    public static class NewsViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        public NewsViewHolder(View itemView){
+            super(itemView);
+            mView = itemView;
+        }
+        public void setTitle(String title){
+            TextView post_title = (TextView)mView.findViewById(R.id.post_title);
+            post_title.setText(title);
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -103,6 +148,18 @@ public class Tab2 extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mPeopleRVAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mPeopleRVAdapter.stopListening();
     }
 
     /**
